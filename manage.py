9a1b7e381044e77ff96b3b7b8c0ec0b936fa52ac
MD Mobassher Rashedi <44574605+MobassherRashedi@users.noreply.github.com>
2022-07-27 17:06:38 +0600

    update all config

--- manage.py
+++ manage.py
@@ -5,8 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mealmanagement.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -14,8 +13,7 @@ def main():
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-        )from exc
-     
+        ) from exc
     execute_from_command_line(sys.argv)
 
 
